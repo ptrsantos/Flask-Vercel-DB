76cf71c4ccfca8b8a8dfe7317fc8eb511330c60a
ptrsantos <ptrsantos@gmail.com>
2025-03-20 18:30:22 -0300

    Adding Files

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://default:2JVBgN6fdZEi@ep-small-cell-a48v87ie-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://Flask-Vercel-DB_owner:npg_nY7Uzrk8jpJW@ep-royal-pine-acra8qhx-pooler.sa-east-1.aws.neon.tech/Flask-Vercel-DB?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://Flask-Vercel-DB_owner:npg_nY7Uzrk8jpJW@ep-royal-pine-acra8qhx-pooler.sa-east-1.aws.neon.tech/Flask-Vercel-DB?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # or another method to set a secret key
